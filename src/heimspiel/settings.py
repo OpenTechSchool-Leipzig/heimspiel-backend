abc9828f16a6cbeaec0318b0f2c8262658aa21d7
Dominik Kellner <dkellner@dkellner.de>
2020-03-21 17:27:20 +0100

    Use custom user model (recommended for new projects)

--- src/heimspiel/settings.py
+++ src/heimspiel/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'heimspiel_auth',
 ]
 
 MIDDLEWARE = [
@@ -84,6 +85,7 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+AUTH_USER_MODEL = 'heimspiel_auth.User'
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
---
+++ src/heimspiel_auth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HeimspielAuthConfig(AppConfig):
+    name = 'heimspiel_auth'
