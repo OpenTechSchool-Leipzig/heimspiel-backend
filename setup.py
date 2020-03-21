57080a2acc2864cf150a314e0575240586b0e0d7
Dominik Kellner <dkellner@dkellner.de>
2020-03-21 16:57:41 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="heimspiel-backend",
+    version="0.1",
+    packages=find_packages(),
+
+    install_requires=[
+        "Django>=3.0.4"
+    ],
+
+    author="OpenTechSchool Leipzig",
+)
