098e26d3778d28ec772121a6593ac1ecaf276e0f
Norma Driske <norma@entwicklerheld.de>
2020-03-22 01:57:39 +0100

    Add base models

--- src/heimspiel_core/admin.py
+++ src/heimspiel_core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
-from .models import Quest, QuestCategory
+from .models import Badge, Player, PlayerAttribute, Quest, QuestCategory, QuestReport
 
+admin.site.register(PlayerAttribute)
+admin.site.register(Player)
 admin.site.register(Quest)
 admin.site.register(QuestCategory)
+admin.site.register(QuestReport)
+admin.site.register(Badge)
--- src/heimspiel_core/models.py
+++ src/heimspiel_core/models.py
@@ -1,10 +1,31 @@
 from django.db import models
 
 
+class PlayerAttribute(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    name = models.CharField(max_length=64)
+    score = models.PositiveIntegerField(default=0)
+    background_story = models.TextField(blank=True)
+    attributes = models.ManyToManyField(PlayerAttribute)
+    # TODO: badges
+
+    def __str__(self):
+        return self.name
+
+
 class QuestCategory(models.Model):
     id = models.CharField(primary_key=True, max_length=16)
     title = models.CharField(max_length=64)
     # TODO: image
+
+    def __str__(self):
+        return self.title
 
 
 class Quest(models.Model):
@@ -14,3 +35,23 @@ class Quest(models.Model):
     flavor_text = models.TextField()
     score = models.IntegerField()
     # TODO: image
+
+    def __str__(self):
+        return self.title
+
+
+class QuestReport(models.Model):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    quests = models.ManyToManyField(Quest)
+
+    def __str__(self):
+        return f"Report of {self.player} ({self.date})"
+
+
+class Badge(models.Model):
+    name = models.CharField(max_length=64)
+    # TODO: constraints
+
+    def __str__(self):
+        return self.name
