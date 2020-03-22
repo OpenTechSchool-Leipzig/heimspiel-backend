212f56210550b203333a5f5b0fc0f64301b3045f
Dominik Kellner <dkellner@dkellner.de>
2020-03-22 10:25:53 +0100

    fixup! Import complete drf to enable browseable api

--- src/heimspiel/settings.py
+++ src/heimspiel/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'heimspiel_auth',
     'heimspiel_core',
 ]
