de0d7f6a8acd13b06cf9c9f3d56e1244ce049f4f
Dominik Kellner <dkellner@dkellner.de>
2020-04-03 12:16:44 +0200

    Fix setup.py ~> `python setup.py check` passes now

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="heimspiel-backend",
-    version="0.1",
-    packages=find_packages(),
+    name='heimspiel-backend',
+    version='0.1',
+    url='https://github.com/OpenTechSchool-Leipzig/heimspiel-backend',
+    author='OpenTechSchool Leipzig',
+    author_email='leipzig@opentechschool.org',
+
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
 
     install_requires=[
-        "Django>=3.0.4",
-        "django-cors-headers>=3.2.1",
-        "django-filer>=1.7.0",
-        "djangorestframework>=3.11.0",
+        'Django>=3.0.4',
+        'django-cors-headers>=3.2.1',
+        'django-filer>=1.7.0',
+        'djangorestframework>=3.11.0',
     ],
-
-    author="OpenTechSchool Leipzig",
 )
