9110d8881f75f94c903853a9be3d6bc95fdbf04e
Norma Driske <norma@fastmail.com>
2020-04-22 18:28:26 +0200

    Add packages for production

--- setup.py
+++ setup.py
@@ -13,6 +13,9 @@ setup(
         "django-cors-headers>=3.2.1",
         "django-filer>=1.7.0",
         "djangorestframework>=3.11.0",
+        "gunicorn==20.0.4",
+        "dj-database-url==0.5.0",
+        "psycopg2-binary==2.8.5",
     ],
     extras_require={"test": ["black>=19.10b0"],},
 )
