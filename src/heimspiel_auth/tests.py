5234bc87b431847946331374ddd7af34aa1f8cfb
Dominik Kellner <dkellner@dkellner.de>
2020-04-22 16:49:19 +0200

    Use `APITestCase` for `AuthTestCase`

--- src/heimspiel_auth/tests.py
+++ src/heimspiel_auth/tests.py
@@ -1,18 +1,15 @@
 import uuid
 from unittest.mock import patch
 
-from django.test import Client, TestCase
 from rest_framework.authtoken.models import Token
+from heimspiel_core.tests import APITestCase
 
 from .serializers import UserSerializer
 
 SAMPLE_UUID = uuid.uuid4()
 
 
-class AuthTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-
+class AuthTestCase(APITestCase):
     @patch("uuid.uuid4", lambda: SAMPLE_UUID)
     def test_create_user(self):
         """Creates a user with a new UUID and an authentication token."""
@@ -29,9 +26,7 @@ class AuthTestCase(TestCase):
     def test_post_user_id_gets_ignored(self):
         """IDs are always generated server-side."""
         our_id = uuid.uuid4()
-        response = self.client.post("/users/", {"id": our_id, "name": "Heimspiel"})
-        self.assertEqual(response.status_code, 201)
-        body = response.json()
+        body = self.post("/users/", {"id": str(our_id), "name": "Heimspiel"})
         self.assertNotEqual(body["id"], our_id)
 
     def test_post_user_returns_token(self):
